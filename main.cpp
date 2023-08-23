a09ca752d8901abb2302037ce3ea60a65dcafbe4
natseche <143021073+natseche@users.noreply.github.com>
2023-08-23 12:35:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -79,30 +79,30 @@ void comandos()
 
 void ayuda()
 {
-    cout << endl << MAGENTA << " » CONFIGURACIÓN DEL JUEGO « " << endl;
-    cout <<endl << BLUE << "--- inicializar --- \n" << WHITE << " ▻ Realiza las operaciones necesarias para inicializar el juego, de acuerdo a las instrucciones entregadas" << endl;
-    cout <<endl << BLUE << "--- turno <id_jugador> --- \n" << WHITE << " ▻ Realiza las operaciones descritas dentro del turno de un jugador (obtener nuevas unidades, atacar y forticar)." << endl;
-    cout << endl << MAGENTA << " » ALMACENAMIENTO DE PARTIDAS « " << endl;
-    cout <<endl << BLUE << "--- salir --- \n" << WHITE<< " ▻ Termina la ejecución de la aplicación." << endl;
-    cout <<endl << BLUE << "--- guardar <nombre_archivo> --- \n" << WHITE << " ▻ El estado actual del juego es guardado en un archivo de texto." << endl;
-    cout <<endl << BLUE << "--- guardar_comprimido <nombre_archivo> --- \n" << WHITE << " ▻ El estado actual del juego es guardado en un archivo binario." << endl;
-    cout <<endl << BLUE << "--- inicializar <nombre_archivo> --- \n" << WHITE << " ▻ Inicializa el juego con los datos contenidos en el archivo identicado por <nombre_archivo>." << endl;
-    cout << endl << MAGENTA << " » ESTRATEGIAS DE JUEGO « " << endl;
-    cout <<endl << BLUE << "--- costo_conquista <territorio> --- \n" << WHITE << " ▻ Se calcula el costo y la secuencia de territorios a ser conquistados para lograr controlar el territorio dado por el usuario. " << endl;
-    cout <<endl << BLUE << "--- conquista_mas_barata --- \n" << WHITE << " ▻ De todos los territorios posibles, calcular aquel que pueda implicar un menor número de unidades de ejército perdidas" << endl;
+    cout << endl << " » CONFIGURACIÓN DEL JUEGO « " << endl;
+    cout <<endl << "--- inicializar --- \n" << " ▻ Realiza las operaciones necesarias para inicializar el juego, de acuerdo a las instrucciones entregadas" << endl;
+    cout <<endl <<"--- turno <id_jugador> --- \n" << " ▻ Realiza las operaciones descritas dentro del turno de un jugador (obtener nuevas unidades, atacar y forticar)." << endl;
+    cout << endl << " » ALMACENAMIENTO DE PARTIDAS « " << endl;
+    cout <<endl << "--- salir --- \n" << " ▻ Termina la ejecución de la aplicación." << endl;
+    cout <<endl << "--- guardar <nombre_archivo> --- \n" << " ▻ El estado actual del juego es guardado en un archivo de texto." << endl;
+    cout <<endl << "--- guardar_comprimido <nombre_archivo> --- \n" << " ▻ El estado actual del juego es guardado en un archivo binario." << endl;
+    cout <<endl << "--- inicializar <nombre_archivo> --- \n" <<" ▻ Inicializa el juego con los datos contenidos en el archivo identicado por <nombre_archivo>." << endl;
+    cout << endl << " » ESTRATEGIAS DE JUEGO « " << endl;
+    cout <<endl << "--- costo_conquista <territorio> --- \n" << " ▻ Se calcula el costo y la secuencia de territorios a ser conquistados para lograr controlar el territorio dado por el usuario. " << endl;
+    cout <<endl << "--- conquista_mas_barata --- \n" << " ▻ De todos los territorios posibles, calcular aquel que pueda implicar un menor número de unidades de ejército perdidas" << endl;
 }
 
 bool inicializar(bool b)
 {
     if(b)
-        cout<<endl << CYAN <<"El juego ya ha sido inicializado."<< WHITE <<endl;
+        cout<<endl << "El juego ya ha sido inicializado."<< endl;
     else
     {
         b = true;
         leerJugadores();
         partida->asignarTerritorios();
         
-        cout<<endl << CYAN <<"El juego se ha inicializado correctamente."<< WHITE << endl;
+        cout<<endl << "El juego se ha inicializado correctamente."<< endl;
     }
     return b;
 }
@@ -113,17 +113,17 @@ void leerJugadores(){
         llenarColores(colores);
         do
         {
-            cout<<endl<< CYAN <<"Ingrese la cantidad de jugadores (3-6): " << WHITE;
+            cout<<endl<< "Ingrese la cantidad de jugadores (3-6): ";
             cin>>cantJugadores;
             if(cantJugadores<3 || cantJugadores>6)
             {
-                cout <<endl << BOLD_MAGENTA << "Ingresó un valor inválido, inténtelo de nuevo"<< WHITE << endl;
+                cout <<endl << "Ingresó un valor inválido, inténtelo de nuevo"<< endl;
             }
         }
         while(cantJugadores<3 || cantJugadores>6);
         for(int i=0; i<cantJugadores; i++)
         {
-            cout<< endl <<CYAN << "Ingrese el nombre o identificador del jugador "<< WHITE<<i+1<<": ";
+            cout<< endl << "Ingrese el nombre o identificador del jugador "<< i+1<<": ";
             cin>>nombre;
             color = asignarColor(cantJugadores, colores);
           
@@ -161,18 +161,18 @@ string asignarColor(int cantJug, list<string> * colores)
 {
     string col;
     bool encontrado = false;
-    cout << endl << CYAN << "Los colores disponibles son: " << WHITE <<endl;
+    cout << endl << "Los colores disponibles son: " << endl;
     for(string color : *colores)
         cout << endl << color;
     do{
-      cout << endl << CYAN << "\nEscoga el color: " << WHITE;
+      cout << endl << "\nEscoga el color: ";
       cin >> col;
       for(string color : *colores){
         if(col == color)
           encontrado = true;
       }
       if(!encontrado)
-        cout << endl << BOLD_MAGENTA<< "Ingresó un color erróneo, inténtelo de nuevo"<< WHITE;
+        cout << endl << "Ingresó un color erróneo, inténtelo de nuevo";
     }while(!encontrado);
     auto it = colores->begin();
     while (it != colores->end())
@@ -192,13 +192,13 @@ bool inicializarArchivo(bool b, string s)
 {
     if(b)
     {
-        cout<<endl<< BOLD_MAGENTA << "El juego ya ha sido inicializado."<<WHITE<< endl;
+        cout<<endl<< "El juego ya ha sido inicializado."<< endl;
     }
     else
     {
         b = true;
         string nombreArchivo = getNombreArchivo(s);
-        cout<<endl<< BOLD_MAGENTA<<"'"<<nombreArchivo<<"'"<<" no contiene información válida para inicializar el juego."<< WHITE<<endl;
+        cout<<endl<< "'"<<nombreArchivo<<"'"<<" no contiene información válida para inicializar el juego."<< endl;
     }
     return b;
 }
@@ -215,11 +215,11 @@ void turno(bool b, string s)
       else{   
         partida->turno(cantJugadores);
       }
-        cout<<endl << CYAN <<"El turno del jugador " << id << " ha terminado."<< WHITE << endl;
+        cout<<endl << "El turno del jugador " << id << " ha terminado."<< endl;
     }
     else
     {
-        cout<<endl << BOLD_MAGENTA <<"Esta partida no ha sido inicializada correctamente."<< WHITE << endl;
+        cout<<endl << "Esta partida no ha sido inicializada correctamente."<< endl;
 
       
     }
@@ -231,11 +231,11 @@ void conquista_barata(bool juego)
 {
     if(juego==false)
     {
-        cout<<endl<< BOLD_MAGENTA <<"El juego no ha sido inicializado correctamente"<<WHITE << endl;
+        cout<<endl<< "El juego no ha sido inicializado correctamente"<< endl;
     }
     else
     {
-        cout<<endl << BOLD_MAGENTA <<"Jugador no válido"<< WHITE << endl;
+        cout<<endl << "Jugador no válido"<< endl;
     }
 }
 
@@ -243,11 +243,11 @@ void costo_conquista(bool b)
 {
     if(b)
     {
-        cout<<endl<< CYAN <<"Para conquistar el territorio <territorio>, debe atacar desde <territorio_1>, pasando por los territorios <territorio_2>, <territorio_3>, ..., <territorio_m>. Debe conquistar<n>unidades de ejército."<< WHITE << endl;
+        cout<<endl<< "Para conquistar el territorio <territorio>, debe atacar desde <territorio_1>, pasando por los territorios <territorio_2>, <territorio_3>, ..., <territorio_m>. Debe conquistar<n>unidades de ejército."<< endl;
     }
     else
     {
-        cout<<endl<< BOLD_MAGENTA <<"Esta partida no ha sido inicializada correctamente."<< WHITE << endl;
+        cout<<endl<< "Esta partida no ha sido inicializada correctamente."<< endl;
     }
 }
 
@@ -259,17 +259,17 @@ void guardar(bool b, string s, fstream& txt)
         txt.open(nombreArchivo, ios::out);
         if(!txt)
         {
-            cout<<endl<< BOLD_MAGENTA <<"La partida no ha sido guardada correctamente."<< WHITE << endl;
+            cout<<endl<< "La partida no ha sido guardada correctamente."<< endl;
         }
         else
         {
-            cout<<endl << CYAN <<"La partida ha sido guardada correctamente en '" << nombreArchivo << "'"<< WHITE << endl;
+            cout<<endl << "La partida ha sido guardada correctamente en '" << nombreArchivo << "'"<< endl;
         }
         txt.close();
     }
     else
     {
-        cout<<endl<< BOLD_MAGENTA <<"Esta partida no ha sido inicializada correctamente."<< WHITE << endl;
+        cout<<endl<< "Esta partida no ha sido inicializada correctamente."<< endl;
     }
 }
 
@@ -281,17 +281,17 @@ void guardar_comprimido(bool b, string s, fstream& bin)
         bin.open(nombreArchivo,ios::binary |ios::out);
         if(!bin)
         {
-            cout<<endl<< BOLD_MAGENTA <<"La partida no ha sido codificada ni guardada correctamente."<< WHITE << endl;
+            cout<<endl<< "La partida no ha sido codificada ni guardada correctamente."<< endl;
         }
         else
         {
-            cout<<endl<< CYAN <<"La partida ha sido codificada y guardada correctamente en '" << nombreArchivo << "'"<< WHITE << endl;
+            cout<<endl<< "La partida ha sido codificada y guardada correctamente en '" << nombreArchivo << "'"<< endl;
             bin.close();
         }
     }
     else
     {
-        cout<<endl << BOLD_MAGENTA <<"Esta partida no ha sido inicializada correctamente."<< WHITE << endl;
+        cout<<endl << "Esta partida no ha sido inicializada correctamente."<< endl;
     }
 }
 
