ebb7db7e081e4d9747a58fe69e6ab9a28787839f
natseche <143021073+natseche@users.noreply.github.com>
2023-08-23 12:26:36 -0500

    Update Partida.h

--- Partida.h
+++ Partida.h
@@ -14,14 +14,23 @@ public:
   Partida();
   void inicializar();
   void turno();
-  void agregarJugador(string,string);
+  void agregarJugador(string,string,int);
   void getJugadores();
   void setContinentes(vector<Continente>);
   void asignarTerritorios();
   void mostrarContinentes();
+  bool verificarContinentes();
+  void escogerTerritorio();
+  int pedirContinente(string);
+  int pedirPais(string, Continente);
   Continente buscarContinente(int);
   vector<Continente> leerContinentes();
   string buscarJugador(string);
-  void ubicarEjercito();
+  void ubicarEjercito(Jugador);
+  int tropasAdicionales(string);
+  void turno(int);
+  void agregarTropasTerr(Jugador);
+  void atacar();
+  int Dado();
 };
 #endif // PARTIDA_H
