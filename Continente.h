c0bf24ffde1a303bf90fa752239621f3441c3051
JulianR <86204935+JRC2004Millos@users.noreply.github.com>
2023-08-24 02:58:37 -0500

    Update Continente.h

--- Continente.h
+++ Continente.h
@@ -4,37 +4,42 @@
 
 using namespace std;
 
-class Continente{
+class Continente
+{
 private:
-  int numero;
-  int contador;
-  string nombre;
-  vector <Pais> paises;
+    int numero;
+    int contador;
+    string nombre;
+    vector <Pais> paises;
 public:
-  Continente();
-  Continente(string);
+    Continente();
+    Continente(string);
 
-  void llenarListaPaises();
-  void agregarPais(Pais);
-  string buscarPais(int);
-  vector <Pais> borrarPais(int);
-  void mostrarPaises();
-  bool paisOcupado(int);
-  string verificarContinente(string);
-  bool verificarPaises();
-  void cambiarTropas(string, int);
-  vector<Pais> obtenerDisponibles();
-  void marcarOcupado(int,string);
+    bool verificarTerritorio(int, string);
+    void llenarListaPaises();
+    void agregarPais(Pais);
+    string buscarPais(int);
+    vector <Pais> borrarPais(int);
+    void mostrarPaises();
+    bool paisOcupado(int);
+    string verificarContinente(string);
+    bool verificarPaises();
+    void sumarTropas(int, int);
+    void restarTropas(int, int);
+    vector<Pais> obtenerDisponibles();
+    void marcarOcupado(int,string);
+    void mostrarVecinos(int);
 
-  int getCantiPaises();
-  void setContador(int);
-  int getContador();
-  int getNumero();
-  void setNumero(int);
-  void setNombre(string);
-  string getNombre();
-  vector <Pais> getPaises();
-  void setPaises(vector <Pais>);
+    int getCantiPaises();
+    void setContador(int);
+    int getContador();
+    int getNumero();
+    void setNumero(int);
+    void setNombre(string);
+    string getNombre();
+    vector <Pais> getPaises();
+    void setPaises(vector <Pais>);
+    int getTropas(int);
 
-  string toString();
+    string toString();
 };
