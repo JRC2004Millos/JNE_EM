f50beef8c5eb9ce91d6fc12c350fcba1a9f0c37c
JulianR <86204935+JRC2004Millos@users.noreply.github.com>
2023-08-24 03:00:10 -0500

    Update Pais.h

--- Pais.h
+++ Pais.h
@@ -1,23 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Pais{
+class Pais
+{
 private:
-  string nombre;
-  int numero;
-  string propietario;
-  int tropas;
+    string nombre;
+    int numero;
+    string propietario;
+    int tropas;
+    vector <string> paisesVecinos;
 public:
-  Pais();
-  Pais(string,int);
-  Pais(string,int,int);
-  void setNombre(string);
-  string getNombre();
-  void setNumero(int);
-  int getNumero();
-  void setPropietario(string);
-  string getPropietario();
-  void setTropas(int);
-  int getTropas();
+    Pais();
+    Pais(string,int);
+    Pais(string,int,int);
+
+    void agregarPaisVecino(string);
+    void mostrarPaisesVecinos();
+
+    void setNombre(string);
+    string getNombre();
+    void setNumero(int);
+    int getNumero();
+    void setPropietario(string);
+    string getPropietario();
+    void sumarTropas(int);
+    void restarTropas(int);
+    void setTropas(int);
+    int getTropas();
+
 };
