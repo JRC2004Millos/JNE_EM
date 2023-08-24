8d7dcf684f15616e12656f877d640f0ced502ba1
JulianR <86204935+JRC2004Millos@users.noreply.github.com>
2023-08-24 02:59:50 -0500

    Update Pais.cpp

--- Pais.cpp
+++ Pais.cpp
@@ -2,48 +2,84 @@
 
 using namespace std;
 
-Pais::Pais(){
-  
+Pais::Pais()
+{
+
 }
 
-Pais::Pais(string n, int num, int tropas){
-  this->nombre = n;
-  this->numero = num;
-  this->tropas = tropas;
-}
-Pais::Pais(string n, int num){
-  this->nombre = n;
-  this->numero = num;
-}
-void Pais::setNombre(string n){
-  this->nombre = n;
+Pais::Pais(string n, int num, int tropas)
+{
+    this->nombre = n;
+    this->numero = num;
+    this->tropas = tropas;
 }
 
-string Pais::getNombre(){
-  return this->nombre;
+Pais::Pais(string n, int num)
+{
+    this->nombre = n;
+    this->numero = num;
 }
 
-void Pais::setNumero(int n){
-  this->numero = n;
+void Pais::agregarPaisVecino(string pais)
+{
+    this->paisesVecinos.push_back(pais);
 }
 
-int Pais::getNumero(){
-  return this->numero;
+void Pais::mostrarPaisesVecinos()
+{
+    cout<<endl<<"Fronteras de: "<<this->nombre<<endl;
+    for(int i = 0; i < this -> paisesVecinos.size(); i++)
+        cout <<endl << this->paisesVecinos[i];
+    cout<<endl;
 }
 
-void Pais::setPropietario(string p){
-  this->propietario = p;
+void Pais::setNombre(string n)
+{
+    this->nombre = n;
 }
 
-string Pais::getPropietario(){
-  return this->propietario;
+string Pais::getNombre()
+{
+    return this->nombre;
 }
 
-void Pais::setTropas(int t){
-  this->tropas = t;
+void Pais::setNumero(int n)
+{
+    this->numero = n;
 }
 
-int Pais::getTropas(){
-  return this->tropas;
+int Pais::getNumero()
+{
+    return this->numero;
+}
+
+void Pais::setPropietario(string p)
+{
+    this->propietario = p;
+}
+
+string Pais::getPropietario()
+{
+    return this->propietario;
+}
+
+void Pais::setTropas(int t)
+{
+    this->tropas = t;
+}
+
+int Pais::getTropas()
+{
+    return this->tropas;
+}
+
+void Pais::sumarTropas(int n)
+{
+    this->tropas += n;
+}
+
+void Pais::restarTropas(int n)
+{
+    this->tropas -= n;
 }
 
