ed95665b663da863914103d8846e9d30a7b5a239
JulianR <86204935+JRC2004Millos@users.noreply.github.com>
2023-10-11 14:18:46 -0500

    Update Partida.h

--- Partida.h
+++ Partida.h
@@ -6,30 +6,42 @@
 
 using namespace std;
 
-class Partida{
-private: 
-  vector<Jugador> jugadores;
-  vector<Continente> continentes;
+class Partida
+{
+private:
+    vector<Jugador> jugadores;
+    vector<Continente> continentes;
 public:
-  Partida();
-  void inicializar();
-  void turno(int);
-  void agregarJugador(string,string,int);
-  void getJugadores();
-  void setContinentes(vector<Continente>);
-  void asignarTerritorios();
-  void mostrarContinentes();
-  bool verificarContinentes();
-  void escogerTerritorio();
-  int pedirContinente(string);
-  int pedirPais(string, Continente);
-  Continente buscarContinente(int);
-  vector<Continente> leerContinentes();
-  int buscarJugador(string);
-  void ubicarEjercito(Jugador);
-  int tropasAdicionales(string);
-  void agregarTropasTerr(Jugador);
-  void atacar();
-  int Dado();
+    Partida();
+    void turno(int);
+    void agregarJugador(string,string,int);
+    vector <Jugador> getJugadores();
+    void setContinentes(vector<Continente>);
+    void asignarTerritorios();
+    void mostrarContinentes();
+    bool verificarContinentes();
+    void escogerTerritorio();
+    int pedirContinente(string);
+    int pedirPais(string, Continente);
+    Continente buscarContinente(int);
+    vector<Continente> leerContinentes();
+    int buscarJugador(string);
+    void ubicarEjercito(int);
+    int tropasAdicionales(string);
+    void agregarTropasTerr(int);
+    int pedirTropas(int);
+    string pedirPais(int, string);
+    void atacar(int);
+    void buscarPaisesVecinos(int, int);
+    vector<int> paisAAtacar(string);
+    vector<int> getPosicionesPais(string);
+    char batalla(vector <int>,vector <int>);
+    void fortalecer(int);
+    int verificarTropas(int, string);
+    bool verTropas();
+    int lanzarDado();
+    void asignarUltimasTropas();
+    string pedirTerritorio(int);
+
 };
 #endif // PARTIDA_H
